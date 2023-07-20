ce1c55a58bed34a6ae0ff12ecfe1fc5dc9bfd2a6
coll16 <126190508+coll16@users.noreply.github.com>
2023-07-20 20:40:31 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+import SimpleLightbox from "simplelightbox"
 // Change code below this line
+ 
+const gallery = document.querySelector('.gallery');
 
-console.log(SimpleLightbox);
+const createItems = ({ preview, original, description }) => {
+    return `
+      <li class="gallery__item" style="list-style-type: none;">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+      </li>
+    `;
+  };
+
+  const galleryItemsMarkup = galleryItems
+  .map(item => createItems(item))
+  .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+  const images = gallery.querySelectorAll('.gallery__image');
+images.forEach(image => {
+  const text = image.getAttribute('alt');
+  image.setAttribute('title', text);
+});
